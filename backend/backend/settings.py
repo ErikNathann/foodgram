b961a22ace9ff70fa873540428a8a0fc1f627172
ErikNathann <butterwithbutter@mail.ru>
2025-03-30 20:51:07 +0300

    First work flow

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-secret-key')
 
-DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = True  # os.getenv('DEBUG', 'False')
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
