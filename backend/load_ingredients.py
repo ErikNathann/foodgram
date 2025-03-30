649046986d106f09e8a95537778e590a657b900b
ErikNathann <butterwithbutter@mail.ru>
2025-03-30 22:09:05 +0300

    test

---
+++ backend/load_ingredients.py
@@ -0,0 +1,16 @@
+import json
+from food.models import Ingredient
+
+
+def load_ingredients_from_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        for item in data:
+            ingredient = Ingredient(
+                name=item['name'],
+                measurement_unit=item['measurement_unit']
+            )
+            ingredient.save()
+
+
+load_ingredients_from_json('ingreients.json')
